925a5a20237083846200b3ab37c6a149cf456639
Erik Einarsson <erik.einarsson@piratpartiet.se>
2023-12-01 22:42:16 +0100

    day1 done

--- day_1/solve.go
+++ day_1/solve.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 func readLines(path string) ([]string, error) {
@@ -74,11 +75,42 @@ func p1(inp []string) {
 
 }
 
+func p2(inp []string) {
+	var digits []int
+	for _, line := range inp {
+		line = strings.Replace(line, "one", "o1e", -1)
+		line = strings.Replace(line, "two", "t2w", -1)
+		line = strings.Replace(line, "three", "t3e", -1)
+		line = strings.Replace(line, "four", "f4r", -1)
+		line = strings.Replace(line, "five", "f5e", -1)
+		line = strings.Replace(line, "six", "s6x", -1)
+		line = strings.Replace(line, "seven", "s7n", -1)
+		line = strings.Replace(line, "eight", "e8t", -1)
+		line = strings.Replace(line, "nine", "n9e", -1)
+
+		fmt.Println(line)
+
+		re := regexp.MustCompile("[1-9]")
+		res := re.FindAllString(line, -1)
+		first := res[0]
+		last := res[len(res)-1]
+		digits = append(digits, str_to_int(first+last))
+	}
+
+	res := 0
+	for _, d := range digits {
+		res = res + d
+		fmt.Println(d)
+	}
+	fmt.Println(res)
+
+}
+
 func main() {
 	inp := get_input()
 	fmt.Println("INPUT:")
 	fmt.Println(inp)
 	//fmt.Println("Part 1:")
 	//p1(inp)
-
+	p2(inp)
 }
